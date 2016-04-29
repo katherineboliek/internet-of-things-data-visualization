a04432bd4f0ec7c10dc1f31616c76ddfeb11e13e
katherineboliek <katherineboliek@gmail.com>
2016-04-28 20:51:40 -0400

    Working on Angular tabs.

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,27 @@
 var app = angular.module('myApp', []);
 
-app.controller('ExampleController'), ['$http', function($http) {
+app.controller('ExampleController', ['$http', function($http) {
 
   this.examples = [];
-  this.activeExample = "";
+  // this.activeExample = "";
   var _this = this;
+
+  this.tab = 1;
+
+  this.selectTab = function(setTab) {
+    this.tab = setTab;
+  };
+  this.isSelected = function(checkTab){
+    return this.tab === checkTab;
+  };
 
   $http.get('/js/examples.json')
     .success(function(data){
       _this.examples = data;
-      // console.log("success");
+      //console.log("success");
     })
     .error(function(msg){
       console.log("This request failed.\n");
     });
 
-}];
+}]);
