c7d44d8994a90fbe22e1d5cd59f5c410b4f19013
katherineboliek <katherineboliek@gmail.com>
2016-04-29 15:21:54 -0400

    Most of styling complete.

--- index.js
+++ index.js
@@ -16,15 +16,19 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req,res){
    res.render('index', data);
-  }); //list page
+ }); //intro
+
+  app.get('/examples', function(req,res){
+     res.render('graphic/examples', data);
+   }); //examples
 
 app.get('/connected', function(req,res){
    res.render('graphic/connected', data);
-  });
+ }); //well connected
 
 app.get('/global', function(req,res){
    res.render('graphic/global', data);
-  });
+ }); //global market
 
 app.listen(8080, function(){
     console.log("Listening on port 8080...");
